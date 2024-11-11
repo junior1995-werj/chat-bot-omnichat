e61989ee56ac59f3a2f9abae8718e80e0f6a651b
Junior Werner <juninhowerner31@gmail.com>
2024-11-11 12:05:04 -0300

    coreção

--- handlers/extract.py
+++ handlers/extract.py
@@ -22,10 +22,8 @@ def bag_of_words(writing, words):
         Pega as sentenças que são limpas e cria um pacote de palavras que são usadas 
         para classes de previsão que são baseadas nos resultados que obtivemos treinando o modelo.
     """
-    # tokenize the pattern
     sentence_words = clear_writing(writing)
-    
-    # cria uma matriz de N palavras
+
     bag = [0]*len(words)
     for setence in sentence_words:
         for i, word in enumerate(words):
@@ -36,22 +34,17 @@ def bag_of_words(writing, words):
 
 
 def registrar_interacao(name_movie, movie_obj):
-    # Armazena a pergunta e resposta em uma lista associada ao usuário
     user_history[name_movie].append(movie_obj)
 
 def class_prediction(writing, model):
 
-    # filtra as previsões abaixo de um limite 0.25
     prevision = bag_of_words(writing, words)
     response_prediction = model.predict(np.array([prevision]))[0]
     results = [[index, response] for index, response in enumerate(response_prediction) if response > 0.25]    
     
-    # verifica nas previsões se não há 1 na lista, se não há envia a resposta padrão (anything_else) 
-    # ou se não corresponde a margem de erro
     if "1" not in str(prevision) or len(results) == 0 :
         results = [[0, response_prediction[0]]]
     
-    # classifica por força de probabilidade
     results.sort(key=lambda x: x[1], reverse=True)
     return [{"intent": classes[r[0]], "probability": str(r[1])} for r in results]
 
@@ -77,6 +70,6 @@ def get_response(intents, intents_json, writing):
                         registrar_interacao(movie.name, movie)
                         break
                 except: 
-                    return "Desculpe não consegui encontrar o Filme."
+                    return "Desculpe não entendi."
           
     return result
